d0b387e479a881d5ea544685252c77a7381bdd9d
Souis <souis41@126.com>
2021-02-05 10:41:59 +0800

    memory effective coding

--- scripts/generate_training_data.py
+++ scripts/generate_training_data.py
@@ -26,30 +26,28 @@ def generate_graph_seq2seq_io_data(
     """
 
     num_samples, num_nodes = df.shape
-    data = np.expand_dims(df.values, axis=-1)
-    data_list = [data]
+    data = np.tile(df.values[:, :, None], [1, 1, 1 + int(add_time_in_day) + int(add_day_in_week)])
     if add_time_in_day:
         time_ind = (df.index.values - df.index.values.astype("datetime64[D]")) / np.timedelta64(1, "D")
         time_in_day = np.tile(time_ind, [1, num_nodes, 1]).transpose((2, 1, 0))
-        data_list.append(time_in_day)
+        data[:, :, 1: -1] = time_in_day[:]
     if add_day_in_week:
         day_in_week = np.zeros(shape=(num_samples, num_nodes, 7))
         day_in_week[np.arange(num_samples), :, df.index.dayofweek] = 1
-        data_list.append(day_in_week)
+        data[:, :, :-1] = day_in_week[:]
 
-    data = np.concatenate(data_list, axis=-1)
-    # epoch_len = num_samples + min(x_offsets) - max(y_offsets)
-    x, y = [], []
     # t is the index of the last observation.
     min_t = abs(min(x_offsets))
     max_t = abs(num_samples - abs(max(y_offsets)))  # Exclusive
-    for t in range(min_t, max_t):
-        x_t = data[t + x_offsets, ...]
-        y_t = data[t + y_offsets, ...]
-        x.append(x_t)
-        y.append(y_t)
-    x = np.stack(x, axis=0)
-    y = np.stack(y, axis=0)
+    # epoch_len = num_samples + min(x_offsets) - max(y_offsets)
+    input_dim, output_dim = data.shape[-1], data.shape[-1]
+    x = np.zeros([max_t - min_t + 1, len(x_offsets), num_nodes, input_dim])
+    y = np.zeros([max_t - min_t + 1, len(x_offsets), num_nodes, output_dim])
+
+    for (i, t) in enumerate(range(min_t, max_t)):
+        x[i] = data[t + x_offsets, ...]
+        y[i] = data[t + y_offsets, ...]
+
     return x, y
 
 
